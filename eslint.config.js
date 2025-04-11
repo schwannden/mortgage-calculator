ff13dc4d814af6c3ecde748b405a10544d91579c
Schwannden Kuo <schwannden.kuo@ui.com>
2025-04-11 15:24:20 +0800

    chore: add linting

---
+++ eslint.config.js
@@ -0,0 +1,23 @@
+import pluginVue from 'eslint-plugin-vue'
+import globals from 'globals'
+
+export default [
+  // add more generic rulesets here, such as:
+  // js.configs.recommended,
+  ...pluginVue.configs['flat/recommended'],
+  // ...pluginVue.configs['flat/vue2-recommended'], // Use this if you are using Vue.js 2.x.
+  {
+    rules: {
+      // override/add rules settings here, such as:
+      // 'vue/no-unused-vars': 'error'
+    },
+    languageOptions: {
+      sourceType: 'module',
+      globals: {
+        ...Object.fromEntries(
+          Object.entries(globals.browser).map(([key, value]) => [key.trim(), value])
+        )
+      }
+    }
+  }
+]
